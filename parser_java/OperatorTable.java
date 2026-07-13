eaf2a6855c0f0a58a92bd9322e8366989b99237d
Edinson <edinson.uribe01@gmail.com>
2026-07-13 14:38:19 +0200

    operator table added

--- parser_java/OperatorTable.java
+++ parser_java/OperatorTable.java
@@ -1,3 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OperatorTable {
+    private record OpKey(TokenType op, Type left, Type right) {}
+
+    private static final Map<OpKey, Type> TABLE = new HashMap<>();
+    //run once.
+    static {
+        TABLE.put(new OpKey(TokenType.PLUS, Type.INTEGER, Type.INTEGER), Type.INTEGER);
+        TABLE.put(new OpKey(TokenType.PLUS, Type.REAL, Type.REAL), Type.REAL);
+        TABLE.put(new OpKey(TokenType.PLUS, Type.INTEGER, Type.REAL), Type.REAL);
+        TABLE.put(new OpKey(TokenType.PLUS, Type.REAL, Type.INTEGER), Type.REAL);
+
+        TABLE.put(new OpKey(TokenType.TIMES, Type.INTEGER, Type.INTEGER), Type.INTEGER);
+        TABLE.put(new OpKey(TokenType.TIMES, Type.REAL, Type.REAL), Type.REAL);
+        TABLE.put(new OpKey(TokenType.TIMES, Type.INTEGER, Type.REAL), Type.REAL);
+        TABLE.put(new OpKey(TokenType.TIMES, Type.REAL, Type.INTEGER), Type.REAL);
+    }
+
     public OperatorTable() {}
+
+    public Type resultType(TokenType op, Type left, Type right) {
+        return TABLE.getOrDefault(new OpKey(op, left, right), Type.UNDEFINED);
+    }
 }
